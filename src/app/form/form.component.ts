16defd233cf84bcecc871def7e99f563d253bb21
sutharsanan007 <sutharsananravi@gmail.com>
2023-09-26 00:55:45 -0400

    Learning Template-driven Forms in Angular

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
 
+  onSubmit(f: NgForm) {
+    
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-// This service is cfeated using the Angular CLI command: ng g s services/user
+// This service is created using the Angular CLI command: ng g s services/user
 @Injectable({
   providedIn: 'root'
 })
